e52e0da478eb354482f248a28a97302828fcfec7
JuanCamiloPaezArizaCAMPUS <camipa2305@gmail.com>
2023-08-24 15:07:00 -0500

    feat: :sparkles: Se agregan tanto la coneccion a la base de datos como el limit para las consultas

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import "dotenv/config";
 import express from "express";
+import { limitRequest } from "./Api/V1/config/limit";
 
 const app = express();
 app.use(express.json());
+app.use(limitRequest());
 
 
 
